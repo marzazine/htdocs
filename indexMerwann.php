43c04bb01473294487e6d46baaeafdacc32e6a5f
Merwann Ayed <225a9ujv@protonmail.ch>
2018-03-26 21:25:52 +0200

    Modifications menu HTML
    Modifications menu HTML

--- indexMerwann.php
+++ indexMerwann.php
@@ -1,22 +1,23 @@
 ﻿<!DOCTYPE html>
 <html>
 <head>
-	<title>Wolvenet : Prépare-toi à vouloir wolvenet !</title>
+	<title>WolveNet France || Télécom & Internet</title>
 	<link rel="stylesheet" type="text/css" href="styles/styleMerwann.css">
 </head>
 
 <body>
-<div id="conteneur">
+<div id="menuHaut">
 
     <div class="logo"></div>
 	
-	<div class="element"><a href="">Page 1</a></div>
+	<div class="element-menuHaut"><a href="">Catalogue</a></div>
 
-    <div class="element"><a href="">Page 2</a></div>
+    <div class="element-menuHaut"><a href="">Offres aux particuliers</a></div>
 
-    <div class="element"><a href="">Page 3</a></div>
+    <div class="element-menuHaut"><a href="">Réseau d'entreprise</a></div>
 
 </div>
+
 </body>
 
 </html>
