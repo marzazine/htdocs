082f41f4f6aaca716555d5ae502a8b23d6f67ed2
Merwann Ayed <225a9ujv@protonmail.ch>
2018-03-26 09:24:52 +0200

    Flexbox
    Début flexbox pour élaborer un menu

--- indexMerwann.php
+++ indexMerwann.php
@@ -4,7 +4,17 @@
 	<title>Wolvenet : Prépare-toi à vouloir wolvenet !</title>
 	<link rel="stylesheet" type="text/css" href="styles/css/styleMerwann.css">
 </head>
-<body>
 
+<body>
+<div id="conteneur">
+
+    <div class="element">Elément 1</div>
+
+    <div class="element">Elément 2</div>
+
+    <div class="element">Elément 3</div>
+
+</div>
 </body>
+
 </html>
