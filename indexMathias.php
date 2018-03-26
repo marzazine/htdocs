8fbf11cb0a4e6da488e6db22754fef3d91c12450
Zeebiii <mathias.msl@outlook.fr>
2018-03-26 09:50:27 +0200

    Debut - Création menu déroulant
    C'est tendu mdr

--- indexMathias.php
+++ indexMathias.php
@@ -1,29 +1,68 @@
 ﻿<!DOCTYPE html>
 <html>
 <head>
-	<title>WolveNet</title>
+	<title>WolveNet - Acceuil</title>
 	<meta charset="utf-8">
 	<link rel="stylesheet" type="text/css" href="styles/css/styleMathias.css	">
 </head>
 <body>
-	<h1>WolveNet</h1>	
+	<h1>WolveNet- Acceuil</h1>	
 	</br>
 	<h2>Prépare-toi à vouloir wolvenet !</h2>
 
-		
+		<nav>
+			<ul>
+
+				<li class="menu-forfait">
+					<a href="forfaitMathias.php">Forfaits</a>
+					<ul>
+						<li>
+							
+						</li>
+					</ul>
+
+
+				</li>
+
+
+				<li class="menu-mobile">
+					<a href="mobilesMathias.php">Mobiles</a>
+					<ul>
+						<li>
+							
+						</li>
+					</ul>
+
+
+				</li>
+				
+
+
+				<li class="menu-boxtv">
+					<a href="boxtvMathias.php">Box/TV</a>
+					<ul>
+						<li>
+							
+						</li>
+					</ul>
+
+				</li>
+
+
+				<li class="menu-offres">
+					<a href="offresMathias.php">Offres</a>
+					<ul>
+						<li>
+							
+						</li>
+					</ul>
+
+				</li>
 
 
 
-
-
-
-
-
-
-
-
-
-
+			</ul>
+		</nav>
 
 </body>
 </html>
--- mobilesMathias.php
+++ mobilesMathias.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>WolveNet</title>
+	<title>WolveNet- Mobiles</title>
 	<meta charset="utf-8">
 	<link rel="stylesheet" type="text/css" href="styles/css/styleMathias.css	">
 </head>
 <body>
-	<h1>WolveNet</h1>	
+	<h1>WolveNet - Mobiles</h1>	
 	</br>
 	<h2>Prépare-toi à vouloir wolvenet !</h2>
 
