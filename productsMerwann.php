3ce12a12aba4ef2c88928f1c2e6d462c60c5d5ba
Merwann Ayed <225a9ujv@protonmail.ch>
2018-03-27 22:50:56 +0200

    Include "top.php"
    Ajout de la fonction "include" pour obtenir un code plus propre

--- productsMerwann.php
+++ productsMerwann.php
@@ -1,25 +1,6 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>WolveNet France || Télécom & Internet</title>
-	<link rel="stylesheet" type="text/css" href="styles/styleMerwann.css">
-	<link rel="icon" type="image/png" href="/img/favicon.png" />
-	<!--[if IE]><link rel="shortcut icon" type="image/x-icon" href="/img/favicon.ico" /><![endif]-->
-</head>
-
-<body>
-<div id="menuHaut">
-
-    <div class="element-logo"><a href="indexMerwann.php"><img src="/img/wolf-mini.png"></a></div>
-	<div class="element-logo2"><img src="/img/logoecrit-mini.png"></div>
-	
-	<div class="element-menuHaut"><a href="productsMerwann.php">Catalogue</a></div>
-
-    <div class="element-menuHaut"><a href="">Offres aux particuliers</a></div>
-
-    <div class="element-menuHaut"><a href="">Réseau d'entreprise</a></div>
-
-</div>
+<?php
+include 'includes/top.php';
+?>
 
 <div id="sousMenuHaut">
 
