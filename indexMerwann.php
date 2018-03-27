7e4e1fabe2779c33d2b7ca8f8eea0fd695f6e501
Merwann Ayed <225a9ujv@protonmail.ch>
2018-03-27 22:24:37 +0200

    Suppression caractères inutiles
    Suppression caractères inutiles

--- indexMerwann.php
+++ indexMerwann.php
@@ -13,11 +13,11 @@
     <div class="element-logo"><a href=""><img src="/img/wolf-mini.png"></a></div>
 	<div class="element-logo2"><img src="/img/logoecrit-mini.png"></div>
 	
-	<div class="element-menuHaut"><a href="productsMerwann.php">Catalogue</a> | </div>
+	<div class="element-menuHaut"><a href="productsMerwann.php">Catalogue</a></div>
 
-    <div class="element-menuHaut"><a href="">Offres aux particuliers</a> | </div>
+    <div class="element-menuHaut"><a href="">Offres aux particuliers</a></div>
 
-    <div class="element-menuHaut"><a href="">Réseau d'entreprise</a> | </div>
+    <div class="element-menuHaut"><a href="">Réseau d'entreprise</a></div>
 
 </div>
 
--- productsMerwann.php
+++ productsMerwann.php
@@ -13,21 +13,21 @@
     <div class="element-logo"><a href="indexMerwann.php"><img src="/img/wolf-mini.png"></a></div>
 	<div class="element-logo2"><img src="/img/logoecrit-mini.png"></div>
 	
-	<div class="element-menuHaut"><a href="productsMerwann.php">Catalogue</a> | </div>
+	<div class="element-menuHaut"><a href="productsMerwann.php">Catalogue</a></div>
 
-    <div class="element-menuHaut"><a href="">Offres aux particuliers</a> | </div>
+    <div class="element-menuHaut"><a href="">Offres aux particuliers</a></div>
 
-    <div class="element-menuHaut"><a href="">Réseau d'entreprise</a> | </div>
+    <div class="element-menuHaut"><a href="">Réseau d'entreprise</a></div>
 
 </div>
 
 <div id="sousMenuHaut">
 
-	<div class="element-sousMenuHaut"><a href="">Téléphones portables</a></div>
+	»<div class="element-sousMenuHaut"><a href="" id="basLien">Téléphones portables</a></div>
 
-    <div class="element-sousMenuHaut"><a href="">Téléphones fixes</a></div>
+    <div class="element-sousMenuHaut"><a href="" id="basLien">Téléphones fixes</a></div>
 
-    <div class="element-sousMenuHaut"><a href="">Accessoires</a> </div>
+    <div class="element-sousMenuHaut"><a href="" id="basLien">Accessoires</a> </div>
 
 </div>
 
