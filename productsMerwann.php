e65111d3b56e9a62891e67b1c3da5c2bb7aa28b6
Merwann Ayed <225a9ujv@protonmail.ch>
2018-03-27 21:00:35 +0200

    Création page catalogue
    Création page catalogue

---
+++ productsMerwann.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>WolveNet France || Télécom & Internet</title>
+	<link rel="stylesheet" type="text/css" href="styles/styleMerwann.css">
+</head>
+
+<body>
+<div id="menuHaut">
+
+    <div class="element-logo"><a href="indexMerwann.php"><img src="/img/wolf-mini.png"></a></div>
+	<div class="element-logo2"><img src="/img/logoecrit-mini.png"></div>
+	
+	<div class="element-menuHaut"><a href="productsMerwann.php">Catalogue</a> | </div>
+
+    <div class="element-menuHaut"><a href="">Offres aux particuliers</a> | </div>
+
+    <div class="element-menuHaut"><a href="">Réseau d'entreprise</a> | </div>
+
+</div>
+
+<div id="sousMenuHaut">
+
+	<div class="element-sousMenuHaut"><a href="">Téléphones portables</a></div>
+
+    <div class="element-sousMenuHaut"><a href="">Téléphones fixes</a></div>
+
+    <div class="element-sousMenuHaut"><a href="">Accessoires</a> </div>
+
+</div>
+
+
+
+</body>
+
+</html>
