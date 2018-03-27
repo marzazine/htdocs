55fd90a3b30431b26181502f9aa0a0e0dd62ecd8
Merwann Ayed <225a9ujv@protonmail.ch>
2018-03-27 20:46:20 +0200

    Amélioration du menu HTML
    Amélioration du menu HTML

--- indexMerwann.php
+++ indexMerwann.php
@@ -8,9 +8,10 @@
 <body>
 <div id="menuHaut">
 
-    <div class="logo"></div>
+    <div class="element-logo"><a href=""><img src="/img/wolf-mini.png"></a></div>
+	<div class="element-logo2"><img src="/img/logoecrit-mini.png"></div>
 	
-	<div class="element-menuHaut"><a href="">Catalogue</a> | </div>
+	<div class="element-menuHaut"><a href="productsMerwann.php">Catalogue</a> | </div>
 
     <div class="element-menuHaut"><a href="">Offres aux particuliers</a> | </div>
 
