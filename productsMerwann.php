f3257bf27ea37c33692729c7c19a26b4d2208210
Merwann Ayed <225a9ujv@protonmail.ch>
2018-03-27 23:45:05 +0200

    Introduction méthode "GET"
    Introduction méthode GET

--- productsMerwann.php
+++ productsMerwann.php
@@ -4,11 +4,12 @@ include 'includes/top.php';
 
 <div id="sousMenuHaut">
 
-	»<div class="element-sousMenuHaut"><a href="" id="basLien">Téléphones portables</a></div>
+	»<div class="element-sousMenuHaut"><a href="productsMerwann.php?categorie=mobiles" id="basLien">Téléphones portables</a></div>
 
-    <div class="element-sousMenuHaut"><a href="" id="basLien">Téléphones fixes</a></div>
+    <div class="element-sousMenuHaut"><a href="productsMerwann.php?categorie=fixes" id="basLien">Téléphones fixes</a></div>
 
-    <div class="element-sousMenuHaut"><a href="" id="basLien">Accessoires</a> </div>
+    <div class="element-sousMenuHaut"><a href="productsMerwann.php?categorie=accessoires" id="basLien">Accessoires</a> </div>
+	
 
 </div>
 
