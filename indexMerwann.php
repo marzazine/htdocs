3aa535ceb8a1a41f8fed75e907aa7bedf8cacf92
Merwann Ayed <225a9ujv@protonmail.ch>
2018-03-28 16:10:25 +0200

    Ajout iPhone X sur page d'accueil
    Ajout iPhone X sur page d'accueil

--- indexMerwann.php
+++ indexMerwann.php
@@ -9,7 +9,10 @@ include 'includes/top.php';
 	<div class="element-bandeau">Profitez de nos offres exceptionnelles
 	<button class="button2" style="vertical-align:middle" onclick="javascript:location.href='offersMerwann.php'"><span>Découvrir </span></button>
 	</div>
+	
+	<span class="iphoneXtexte2">iPhone</span><div id="iphoneXlogo"><img src="/img/X.png"></div>
 	<div class="iphoneX"><img src="/img/iphoneX.png"></div>
+	<button class="button3" style="vertical-align:middle" onclick="javascript:location.href='offersMerwann.php'"><span class="iphoneXtexte">à partir de 1€* </span></button>
 </div>
 
 
