8b4f21c66f51feeac8022de288162523fc28eb28
Merwann Ayed <225a9ujv@protonmail.ch>
2018-03-28 14:57:03 +0200

    Ajout police d'écriture

--- indexMerwann.php
+++ indexMerwann.php
@@ -5,7 +5,8 @@ include 'includes/top.php';
 <body>
 
 <div id="bandeau">
-<img src="/img/body-bg.jpg">
+	<img src="/img/body-bg.jpg">
+	<div class="element-bandeau">Profitez de nos offres exceptionnelles</div>
 </div>
 
 
