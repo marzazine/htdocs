db10aee2d9be6f67707dd87750de178ee82ef2d1
Merwann Ayed <225a9ujv@protonmail.ch>
2018-03-28 16:27:51 +0200

    Chacun son top.php avec son propre CSS
    Chacun son top.php avec son propre CSS

---
+++ includes/topNoam.php
@@ -0,0 +1,22 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+	<title>WolveNet France || Télécom & Internet</title>
+	<link rel="stylesheet" type="text/css" href="../styles/styleNoam.css">
+	<link rel="icon" type="image/png" href="../img/favicon.png" />
+	<!--[if IE]><link rel="shortcut icon" type="image/x-icon" href="../img/favicon.ico" /><![endif]-->
+</head>
+
+<div id="menuHaut">
+
+    <div class="element-logo"><a href="indexMerwann.php"><img src="/img/wolf-mini.png"></a></div>
+	<div class="element-logo2"><img src="/img/logoecrit-mini.png"></div>
+	
+	<div class="element-menuHaut"><a href="productsMerwann.php">Catalogue</a></div>
+
+    <div class="element-menuHaut"><a href="">Offres aux particuliers</a></div>
+
+    <div class="element-menuHaut"><a href="">Réseau d'entreprise</a></div>
+	<div class="element-sousMenuHaut"><button class="button" style="vertical-align:middle" onclick="javascript:location.href='guest.php'"><span>Espace clients </span></button></div>
+
+</div>
