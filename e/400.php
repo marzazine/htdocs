dda7268951b5ee24ff1883f7f6ae052d440aa222
Merwann Ayed <225a9ujv@protonmail.ch>
2018-03-28 22:30:24 +0200

    Personnalisation des erreurs du serveur
    Personnalisation totale des erreurs fréquentes type "Erreur 404"
    
    Manquera plus qu'à interdire l'accès aux dossiers non-dotés d'un "index.php" pour interdire leur lecture.

---
+++ e/400.php
@@ -0,0 +1,11 @@
+<?php
+include '../includes/top.php';
+?>
+<body>
+<br />
+<div id="sectionE">
+Erreur 400 - Bad request (Requête incorrecte)
+<br /><br />
+<a href="/">&#8629; Retourner à l'index</a>
+</div>
+</body>
---
+++ e/500.php
@@ -0,0 +1,3 @@
+<?php
+include '../includes/top.php';
+?>
