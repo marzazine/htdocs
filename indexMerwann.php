9443a8f5a1b5b21e9206da6408fef0fef51a6bbb
Merwann Ayed <225a9ujv@protonmail.ch>
2018-03-28 15:44:30 +0200

    Ajout iphone X
    Ajout iphone X

--- indexMerwann.php
+++ indexMerwann.php
@@ -7,8 +7,9 @@ include 'includes/top.php';
 <div id="bandeau">
 	<img src="/img/body-bg.jpg">
 	<div class="element-bandeau">Profitez de nos offres exceptionnelles
-	<button class="button2" style="vertical-align:middle" onclick="javascript:location.href='guest.php'"><span>Découvrir </span></button>
+	<button class="button2" style="vertical-align:middle" onclick="javascript:location.href='offersMerwann.php'"><span>Découvrir </span></button>
 	</div>
+	<div class="iphoneX"><img src="/img/iphoneX.png"></div>
 </div>
 
 
