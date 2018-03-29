64a8123e6bcca89a084e17e3a8525bf06345f80a
Merwann Ayed <225a9ujv@protonmail.ch>
2018-03-29 13:49:56 +0200

    Personnalisation Erreur 404
    Personnalisation Erreur 404

--- e/404.php
+++ e/404.php
@@ -1,3 +1,11 @@
 <?php
 include '../includes/top.php';
 ?>
+<body>
+<br />
+<div id="sectionE">
+Erreur 404 - File not foud (Fichier non trouvé)
+<br /><br />
+<a href="/">&#8629; Retourner à l'index</a>
+</div>
+</body>
