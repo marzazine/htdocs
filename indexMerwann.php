cb173687519ff03d191cc967ba2ddb4873b1a291
Merwann Ayed <225a9ujv@protonmail.ch>
2018-03-29 20:00:55 +0200

    Finitions cadres "nos offres" puis indentation HTML
    J'ai fini de compléter les cadres "nos offres" avec toutes les informations nécessaires et j'ai indenté le code pour le rendre plus lisible et plus propre.

--- indexMerwann.php
+++ indexMerwann.php
@@ -2,41 +2,53 @@
 include 'includes/top.php';
 ?>
 
-<body>
+    <body>
 
-<div id="bandeau">
-	<img src="/img/body-bg.png">
-	<div class="element-bandeau">Profitez de nos offres exceptionnelles
-	<button class="button2" style="vertical-align:middle" onclick="javascript:location.href='offersMerwann.php'"><span>Découvrir </span></button>
-	</div>
-	
-	<span class="iphoneXtexte2">iPhone</span><div id="iphoneXlogo"><img src="/img/X.png"></div>
-	<div class="iphoneX"><img src="/img/iphoneX.png"></div>
-	<button class="button3" style="vertical-align:middle" onclick="javascript:location.href='offersMerwann.php'"><span class="iphoneXtexte">à partir de 1€* </span></button>
-</div>
+        <div id="bandeau">
+            <img src="/img/body-bg.png">
+            <div class="element-bandeau">Profitez de nos offres exceptionnelles
+                <button class="button2" style="vertical-align:middle" onclick="javascript:location.href='offersMerwann.php'"><span>Découvrir </span></button>
+            </div>
 
-<div id="nosOffresTitre">Nos offres</div>
+            <span class="iphoneXtexte2">iPhone</span>
+            <div id="iphoneXlogo"><img src="/img/X.png"></div>
+            <div class="iphoneX"><img src="/img/iphoneX.png"></div>
+            <button class="button3" style="vertical-align:middle" onclick="javascript:location.href='offersMerwann.php'"><span class="iphoneXtexte">à partir de 1€* </span></button>
+        </div>
 
-<div id="nosOffresForfaitConteneur">
+        <div id="nosOffresTitre">Nos offres</div>
 
-	<div class="nosOffresForfaitBloc">
-		<span class="ForfaitTitre">Forfaits avec mobiles</span>
-		<span class="blocTexte">Un forfait complet adapté à vos besoins et un mobile à prix bon marché !</span>
-		<div id="imgMobile"><img src="/img/mobile-mini.png"></div>
-	</div>
-	
-	<div class="nosOffresForfaitBloc">
-	<span class="ForfaitTitre">Forfaits sans mobile</span>
-	<span class="blocTexte">La liberté du sans engagement avec la qualité de service de Wolvenet.</span>
-	<div id="imgSimcard"><img src="/img/simcard-mini.png"></div></div>
+        <div id="nosOffresForfaitConteneur">
 
-</div>
+            <div class="nosOffresForfaitBloc">
+                <span class="ForfaitTitre">Mobiles</span>
+                <span class="blocTexte">Trouvez le mobile qui vous ressemble le plus et adoptez-le.</span>
+                <div id="imgMobile"><img src="/img/mobile-mini.png"></div>
+                <button class="button-Mobiles" style="vertical-align:middle" onclick="javascript:location.href='#'"><span>Voir les mobiles </span></button>
+            </div>
 
+            <div class="nosOffresForfaitBloc">
+                <span class="ForfaitTitre">Forfaits avec mobiles</span>
+                <span class="blocTexte">Un forfait <strong>complet</strong> adapté à vos besoins et un mobile à prix bon marché.</span>
+                <div id="imgMobileForfait"><img src="/img/mobile-simcard-mini.png"></div>
+                <button class="button-ForfaitMobile" style="vertical-align:middle" onclick="javascript:location.href='#'"><span>Forfaits avec mobile </span></button>
+            </div>
 
+            <div class="nosOffresForfaitBloc">
+                <span class="ForfaitTitre">Forfaits sans mobile</span>
+                <span class="blocTexte">La liberté du <strong>sans engagement</strong> avec la <strong>qualité</strong> de service de Wolvenet.</span>
+                <div id="imgSimcard"><img src="/img/simcard-mini.png"></div>
+				<button class="button-Forfait" style="vertical-align:middle" onclick="javascript:location.href='#'"><span>Forfaits sans mobile </span></button>
+            </div>
 
+            <div class="nosOffresForfaitBloc">
+                <span class="ForfaitTitre">Offres internet</span>
+                <span class="blocTexte">Box internet, Wi-Fi, Fibre optique, ADSL, 4G...</span>
+                <div id="imgInternet"><img src="/img/internet-mini.png"></div>
+				<button class="button-Internet" style="vertical-align:middle" onclick="javascript:location.href='#'"><span>Nos solutions </span></button>
+            </div>
 
+        </div>
+    </body>
 
-
-</body>
-
-</html>
+    </html>
