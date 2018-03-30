10240b88a46095f94f777042bbdfd82207fdf04c
Merwann Ayed <225a9ujv@protonmail.ch>
2018-03-30 10:53:33 +0200

    Ajout page "catalogue"
    Ajout page "catalogue"

---
+++ catalogue/index.php
@@ -0,0 +1,20 @@
+<?php
+include '../includes/top.php';
+?>
+
+<div id="sousMenuHaut">
+
+	<div class="element-sousMenuHaut"><a href="#" id="basLien">Téléphones portables</a></div>
+
+    <div class="element-sousMenuHaut"><a href="#" id="basLien">Téléphones fixes</a></div>
+
+    <div class="element-sousMenuHaut"><a href="#">Accessoires</a> </div>
+	
+
+</div>
+
+
+
+</body>
+
+</html>
--- includes/top.php
+++ includes/top.php
@@ -1,7 +1,7 @@
 ﻿<!DOCTYPE html>
 <html>
 <head>
-	<title>WolveNet France || Télécom & Internet</title>
+	<title>Wolvenet France || Internet, Téléphonie, Mobiles, Forfaits</title>
 	<link rel="stylesheet" type="text/css" href="../styles/styleMerwann.css">
 	<link rel="stylesheet" type="text/css" href="../styles/guest.css">
 	<link rel="stylesheet" type="text/css" href="../styles/index.css">
@@ -15,7 +15,7 @@
     <div class="element-logo"><a href="indexMerwann.php"><img src="/img/wolf-mini.png"></a></div>
 	<div class="element-logo2"><img src="/img/logoecrit-mini.png"></div>
 	
-	<div class="element-menuHaut"><a href="productsMerwann.php">Catalogue</a></div>
+	<div class="element-menuHaut"><a href="/catalogue/">Catalogue</a></div>
 
     <div class="element-menuHaut"><a href="">Offres aux particuliers</a></div>
 
