72c4c1d59ce6a8432ac8f9cbac5ce28fbce25d55
Zeebiii <mathias.msl@outlook.fr>
2018-03-30 15:08:28 +0200

    |TEST| CSS du bouton envoyer "submit".

--- indexMathias.php
+++ indexMathias.php
@@ -1,22 +1,67 @@
 ﻿<?php
-include 'includes/top.php';
+include 'includes/topMathias.php';
 ?>
 
-<body>
+    <body>
 
-<div id="bandeau">
-	<img src="/img/body-bg.jpg">
-	<div class="element-bandeau">Profitez de nos offres exceptionnelles
-	<button class="button2" style="vertical-align:middle" onclick="javascript:location.href='offersMerwann.php'"><span>Découvrir </span></button>
-	</div>
-	
-	<span class="iphoneXtexte2">iPhone</span><div id="iphoneXlogo"><img src="/img/X.png"></div>
-	<div class="iphoneX"><img src="/img/iphoneX.png"></div>
-	<button class="button3" style="vertical-align:middle" onclick="javascript:location.href='offersMerwann.php'"><span class="iphoneXtexte">à partir de 1€* </span></button>
-</div>
+        <div id="bandeau">
+            <img src="/img/body-bg.png">
+            <div class="element-bandeau">Profitez de nos offres exceptionnelles
+                <button class="button2" style="vertical-align:middle" onclick="javascript:location.href='offersMerwann.php'"><span>Découvrir </span></button>
+            </div>
 
+            <span class="iphoneXtexte2">iPhone</span>
+            <div id="iphoneXlogo"><img src="/img/X.png"></div>
+            <div class="iphoneX"><img src="/img/iphoneX.png"></div>
+            <button class="button3" style="vertical-align:middle" onclick="javascript:location.href='offersMerwann.php'"><span class="iphoneXtexte">à partir de 1€* </span></button>
+        </div>
 
+        <div id="TitreAvConteneur">Nos offres</div>
 
-</body>
+        <div id="conteneur">
 
-</html>
+            <div class="bloc">
+                <span class="TitreBloc">Mobiles</span>
+                <span class="blocTexte">Trouvez le mobile qui vous ressemble le plus et adoptez-le.</span>
+                <div id="imgMobile"><img src="/img/mobile-mini.png"></div>
+                <button class="button-Mobiles" style="vertical-align:middle" onclick="javascript:location.href='#'"><span>Voir les mobiles </span></button>
+            </div>
+
+            <div class="bloc">
+                <span class="TitreBloc">Forfaits avec mobiles</span>
+                <span class="blocTexte">Un forfait <strong>complet</strong> adapté à vos besoins et un mobile à prix bon marché.</span>
+                <div id="imgMobileForfait"><img src="/img/mobile-simcard-mini.png"></div>
+                <button class="button-ForfaitMobile" style="vertical-align:middle" onclick="javascript:location.href='#'"><span>Forfaits avec mobile </span></button>
+            </div>
+
+            <div class="bloc">
+                <span class="TitreBloc">Forfaits sans mobile</span>
+                <span class="blocTexte">La liberté du <strong>sans engagement</strong> avec la <strong>qualité</strong> de service de Wolvenet.</span>
+                <div id="imgSimcard"><img src="/img/simcard-mini.png"></div>
+				<button class="button-Forfait" style="vertical-align:middle" onclick="javascript:location.href='#'"><span>Forfaits sans mobile </span></button>
+            </div>
+
+            <div class="bloc">
+                <span class="TitreBloc">Offres internet</span>
+                <span class="blocTexte">Box internet, Wi-Fi, Fibre optique, ADSL, 4G...</span>
+                <div id="imgInternet"><img src="/img/internet-mini.png"></div>
+				<button class="button-Internet" style="vertical-align:middle" onclick="javascript:location.href='#'"><span>Nos solutions </span></button>
+            </div>
+
+        </div>
+		
+		<div id="TitreAvConteneur">Bons plans du moment</div>
+		
+		<div id="conteneur">
+		
+            <div class="blocPromo">
+                <span class="TitreBlocPromo">Spotify Premium</span>
+                <span class="blocTexte">Écoutez votre titres favoris, découvrez les nouveautés et composez la playlist de vos rêves. </span>
+                <div id="imgSpotify"><img src="/img/spotify-mini.png"></div>
+                <button class="button-BonsPlans" style="vertical-align:middle" onclick="javascript:location.href='#'"><span>J'en profite </span></button>
+            </div>
+			
+		</div>
+    </body>
+
+    </html>
