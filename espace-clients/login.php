3ab87f92679169b12f7a982252071cd23e330fa4
Merwann Ayed <225a9ujv@protonmail.ch>
2018-04-02 17:10:22 +0200

    Ajout fichier login.php
    A compléter

---
+++ espace-clients/login.php
@@ -0,0 +1,8 @@
+<?php require ('../inc/functions.php'); ?>
+<?php require ('../inc/header.php'); ?>
+
+    <h1>Se connecter</h1>
+
+<?php debug($_SESSION); ?>
+
+<?php require ('../inc/footer.php'); ?>
