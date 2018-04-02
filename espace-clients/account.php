31ba849fa7d0d777af559f2c7045f958bd6506cd
Merwann Ayed <225a9ujv@protonmail.ch>
2018-04-02 16:58:04 +0200

    Espace client PDO POO

---
+++ espace-clients/account.php
@@ -0,0 +1,9 @@
+<?php require ('../inc/functions.php'); ?>
+<?php session_start(); ?>
+<?php require ('../inc/header.php'); ?>
+
+<h1>Mon espace client</h1>
+
+<?php debug($_SESSION); ?>
+
+<?php require ('../inc/footer.php'); ?>
