df7cfc094d81695b220cbe9e6fcaddf440c3330c
Merwann Ayed <225a9ujv@protonmail.ch>
2018-04-04 17:56:32 +0200

    Création de la page login.php
    Sans PHP pas encore fonctionnelle

--- espace-clients/login.php
+++ espace-clients/login.php
@@ -1,8 +1,26 @@
 <?php require ('../inc/functions.php'); ?>
 <?php require ('../inc/header.php'); ?>
-
-    <h1>Se connecter</h1>
-
 <?php debug($_SESSION); ?>
 
-<?php require ('../inc/footer.php'); ?>
+<body>
+<div id="sectionTitre"><span class="titreForm">Connexion à votre espace client</span></div>
+<div id="section">
+
+<form action="" method="POST">
+	
+    <div id="label1">
+    <label for="email">Votre adresse e-mail<span class="champ-obligatoire">*</span> : </label>
+    <input type="text" name="mail" placeholder="Adresse e-mail" maxlength="100" /><br /><br />
+	</div>
+
+	<div id="label0">
+    <label for="mdp">Votre mot de passe<span class="champ-obligatoire">*</span> : </label>
+    <input type="password" name="password" placeholder="Mot de passe" maxlength="256"/><br /><br />
+	</div>
+
+    <br /><button type="submit">Envoyer</button><br />
+    <span class="champ-obligatoire-tip">* : Champs obligatoires</span>
+</form>
+
+
+<?php require '../inc/footer.php'; ?>
