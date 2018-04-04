ca0280ce2d0948ad72a2713145b32e7c42ede72c
Merwann Ayed <225a9ujv@protonmail.ch>
2018-04-04 20:22:58 +0200

    Possibilité de changer son mot de passe

--- espace-clients/account.php
+++ espace-clients/account.php
@@ -9,7 +9,13 @@ logged_only();
 
 <h1>Votre espace client, <?= $_SESSION['auth']->prenCli; ?></h1>
 
+<form action="" method="POST">
+	<input type="password" name="password" placeholder="Changez votre mot de passe" /><br />
 
+	<input type="password" name="password_confirm" placeholder="Confirmation du mot de passe" /><br />
+	
+	<button type="submit">Envoyer</button>
+</form>
 
 
 <?php require ('../inc/footer.php'); ?>
