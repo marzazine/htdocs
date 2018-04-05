3413f46f852ab8097f599a8e52ad39e4d9484471
Merwann Ayed <225a9ujv@protonmail.ch>
2018-04-05 15:18:04 +0200

    Mise en forme bloc espace client

--- espace-clients/account.php
+++ espace-clients/account.php
@@ -9,13 +9,18 @@ logged_only();
 
 <h1>Votre espace client, <?= $_SESSION['auth']->prenCli; ?></h1>
 
-<form action="" method="POST">
+<div class="section-bloc-general">
+	<div class="section-titre"><span class="titre-section">Mon espace client | </span><span class="texte-normal">Changer mon mot de passe</span></div>
+<div class="section-bloc-contenu">
+	<form action="" method="POST">
 	<input type="password" name="password" placeholder="Changez votre mot de passe" /><br />
 
 	<input type="password" name="password_confirm" placeholder="Confirmation du mot de passe" /><br />
 	
 	<button type="submit">Envoyer</button>
+</div>
 </form>
+</div>
 
 
 <?php require ('../inc/footer.php'); ?>
