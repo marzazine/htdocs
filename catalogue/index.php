0d3948221042c67b7522ad7028daa69db7a90740
Merwann Ayed <225a9ujv@protonmail.ch>
2018-04-06 14:56:38 +0200

    Page "Catalogue" finalisée
    Que l'index !

--- catalogue/index.php
+++ catalogue/index.php
@@ -2,11 +2,11 @@
 
 <div id="sousMenuHaut">
 
-	<div class="element-sousMenuHaut"><a href="#" id="basLien">Mobiles</a> </div>
+	<div class="element-sousMenuHaut"><a href="mobiles.php" id="basLien">Mobiles</a> </div>
 
-    <div class="element-sousMenuHaut"><a href="#" id="basLien">Téléphones fixes</a> </div>
+    <div class="element-sousMenuHaut"><a href="telephones-fixes.php" id="basLien">Téléphones fixes</a> </div>
 
-    <div class="element-sousMenuHaut"><a href="#" id="basLien">Accessoires</a> </div>
+    <div class="element-sousMenuHaut"><a href="accessoires.php" id="basLien">Accessoires</a> </div>
 	
 
 </div>
@@ -20,5 +20,35 @@
 	</span>
 </div>
 </div>
+
+<div id="conteneur">
+
+            <div class="bloc2">
+                <span class="TitreBloc2">Mobiles</span>
+                <span class="blocTexte">Trouvez le mobile qui vous ressemble le plus et adoptez-le.</span>
+                <div id="imgMobile"><img src="/img/mobile-mini.png"></div>
+                <button class="button-catalogue" style="vertical-align:middle" onclick="javascript:location.href='mobiles.php'"><span>Voir les mobiles </span></button>
+            </div>
+
+            <div id="conteneur">
+
+            <div class="bloc2">
+                <span class="TitreBloc2">Téléphones fixes</span>
+                <span class="blocTexte">Des téléphones sans fil simples, fonctionnels, légers et compacts.</span>
+                <div id="imgFixe"><img src="/img/fixe-mini.png"></div>
+                <button class="button-catalogue" style="vertical-align:middle" onclick="javascript:location.href='telephones-fixes.php'"><span>Voir les fixes </span></button>
+            </div>
+
+            <div class="bloc2">
+                <span class="TitreBloc2">Accessoires</span>
+                <span class="blocTexte">Personnalisez vos appareils avec une multitude d'accessoires adaptés.</span>
+                <div id="imgFixe"><img src="/img/airpods-mini.png"></div>
+                <button class="button-catalogue" style="vertical-align:middle" onclick="javascript:location.href='accessoires.php'"><span>Voir les accessoires </span></button>
+            </div>
+
+</div>
+
+
+
 
 <?php require '../inc/footer.php'; ?>
