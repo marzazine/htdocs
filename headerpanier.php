34eab68283f6717649a662371136fa0afa39c818
noam chaalane <noam.chaalane@gmail.com>
2018-04-27 15:11:03 +0200

    modif headerpanier.php
    copie du header sur le headerpanier.php
    pour test

---
+++ headerpanier.php
@@ -0,0 +1,53 @@
+<?php
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Wolvenet France || Internet, Téléphonie, Mobiles, Forfaits</title>
+	<link rel="stylesheet" type="text/css" href="../styles/styleMerwann.css">
+	<link rel="stylesheet" type="text/css" href="../e/e.css">
+
+	<link rel="shortcut icon" type="image/x-png" href="../img/favicon.png" />
+	<!--[if IE]><link rel="shortcut icon" type="image/x-icon" href="../img/favicon.ico" /><![endif]-->
+</head>
+
+<div id="menuHaut">
+
+    <div class="element-logo"><a href="/"><img src="/img/wolf-mini.png"></a></div>
+	<div class="element-logo2"><img src="/img/logoecrit-mini.png"></div>
+	
+	<div class="element-menuHaut"><a href="/catalogue/">Catalogue</a></div>
+
+    <div class="element-menuHaut"><a href="/offres-aux-particuliers/">Offres aux particuliers</a></div>
+
+    <div class="element-menuHaut"><a href="/offres-aux-entreprises/">Réseau d'entreprise</a></div>
+
+    <div class="element-menuHaut"><a href="/assistance/">Assistance</a></div>
+	
+
+	<?php if(isset($_SESSION['auth'])): ?>
+
+		<div class="element-sousMenuHaut"><button class="button" style="vertical-align:middle" onclick="javascript:location.href='/espace-clients/account.php'"><span>Mon espace </span></button></div>
+
+		<div class="element-sousMenuHaut"><button class="button-deco" style="vertical-align:middle" onclick="javascript:location.href='/espace-clients/logoutNoam.php'"><span>Déconnexion </span></button></div>
+
+	<?php else: ?>
+		<div class="element-sousMenuHaut"><button class="button" style="vertical-align:middle" onclick="javascript:location.href='/espace-clients/loginNoam.php'"><span>Connexion espace clients </span></button></div>
+
+		<div class="element-sousMenuHaut"><button class="button-deco" style="vertical-align:middle" onclick="javascript:location.href='/espace-clients/registerNoam.php'"><span>Inscription </span></button></div>
+
+	<?php endif; ?>
+</div>
+
+<?php if(isset($_SESSION['flash'])): ?>
+    <?php foreach ($_SESSION['flash'] as $type => $message): ?>
+        <div class="alert-<?= $type; ?>">
+            <?= $message; ?>
+        </div>
+    <?php endforeach; ?>
+    <?php unset($_SESSION['flash']); ?>
+<?php endif; ?>
