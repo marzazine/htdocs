3d3155e390071d093620606cda7b03e203dbd344
Merwann Ayed <225a9ujv@protonmail.ch>
2018-04-27 13:56:43 +0200

    Afficher table en PDO

--- catalogue/lister.php
+++ catalogue/lister.php
@@ -1,10 +1,22 @@
 <?php 
 require_once '../inc/db.php';
 
-$pdoStat = $pdo->prepare('SELECT * from mobiles LIMIT 2');
+$pdoStat = $pdo->prepare('SELECT * from mobiles');
 
 $executeIsOk = $pdoStat->execute();
 
 $mobiles = $pdoStat->fetchAll();
 
-var_dump($mobiles);
+?>
+
+<ul>
+	<?php foreach ($mobiles as $mobile): ?>
+
+		<li><?= $mobile->marqueMo ?></li>
+		<li><?= $mobile->modeleMo ?></li>
+		<li><?= $mobile->prixbaseMo ?></li>
+		<li><?= $mobile->anneeMo ?></li>
+		<br />
+
+	<?php endforeach; ?>
+<ul>
