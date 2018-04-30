d94827c47617080bcbf9cf543b4fa71ba5654745
Merwann Ayed <225a9ujv@protonmail.ch>
2018-04-30 15:10:11 +0200

    Test affichage grande image du mobile sur viewmobile.php
    Code HTML temporaire en attente de CSS

--- catalogue/viewmobile.php
+++ catalogue/viewmobile.php
@@ -33,15 +33,23 @@ if(!empty($_GET) && !empty($_GET['id'])){
 	</div>
 
 <div class="section-bloc-contenu">
+
+	<center><img src="../img/mobiles/<?= $mobile->urlimgMo ?>"></center>
+	<br />
+	<br />
+	<?= $mobile->descMo ?>
+	<br /><br />
+
 	Marque : <?= $mobile->marqueMo ?> <br /><br />
 	Modèle : <?= $mobile->modeleMo ?><br /><br />
 	Couleur : <?= $mobile->couleurMo ?><br /><br />
 	Prix : <?= $mobile->prixbaseMo ?>€<br /><br />
 	Capacité : Jusqu'à <?= $mobile->capaciteMo ?><br /><br />
-	Description : <?= $mobile->descMo ?><br /><br />
 	Garantie : <?= $mobile->garantieMo ?> <br /><br />
 	Poids : <?= $mobile->poidsMo ?><br /><br />
 	Dimensions : <?= $mobile->dimenMo ?><br /><br />
 
 </div>
 </div>
+
+<center><a href="mobiles.php"><< Retourner voir les autres mobiles</a></center>
