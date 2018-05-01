d71a3b6299c668ed1251d130552a6d5e37c70943
Merwann Ayed <225a9ujv@protonmail.ch>
2018-05-01 15:09:00 +0200

    Amélioration sécurité register.php
    - Amélioration message flash
    - Amélioration expressions régulières (regex) pour accueillir les clients ayant un tiret dans leur nom de famille ou prénom
    - Ajout du champ "complément d'adresse"
    - Ajout HTML du type "required"
    - Ajout des sécurités du champ "complément d'adresse"
    - Désormais le formulaire garde toutes les données saisies au cas où le client se trompe dans le formulaire, il n'a pas à les réécrire.

--- espace-clients/register.php
+++ espace-clients/register.php
@@ -10,14 +10,14 @@ if(empty(!$_POST)){
     $errors = array();
     require_once '../inc/db.php';
 
-    if(empty($_POST['nomdefamille']) || !preg_match('/^[a-zA-Z]+$/', $_POST['nomdefamille'])) {
+    if(empty($_POST['nomdefamille']) || !preg_match('/^[a-zA-Z-]+$/', $_POST['nomdefamille'])) {
 
-        $errors['nomdefamille'] = "Le nom de famille saisi n'est pas valide.";
+        $errors['nomdefamille'] = "Le nom de famille saisi n'est pas valide. (Espaces interdits, optez pour un tiret)";
     }
 
-    if(empty($_POST['prenom']) || !preg_match('/^[a-zA-Z]+$/', $_POST['prenom'])) {
+    if(empty($_POST['prenom']) || !preg_match('/^[a-zA-Z-]+$/', $_POST['prenom'])) {
 
-        $errors['prenom'] = "Le prénom saisi n'est pas valide.";
+        $errors['prenom'] = "Le prénom saisi n'est pas valide. (Espaces interdits, optez pour un tiret)";
     }
 
     if(empty($_POST['mail']) || !filter_var($_POST['mail'], FILTER_VALIDATE_EMAIL)){
@@ -60,6 +60,13 @@ if(empty(!$_POST)){
         }
     }
 
+    $complementadresse_length = strlen($_POST['complement-adresse']);
+
+    if ($complementadresse_length > 255) {
+
+        $errors['complement-adresse'] = "Le complément d'adresse saisi n'est pas valide";
+    }
+
     $codepostal_length = strlen($_POST['codepostal']);
 
     if(empty($_POST['codepostal']) || !preg_match('/^[0-9]+$/', $_POST['codepostal']) || $codepostal_length != 5) {
@@ -97,7 +104,7 @@ if(empty(!$_POST)){
     }
 
     if (empty($errors)){
-        $req = $pdo->prepare("INSERT INTO clients SET pseudoCli = ?, prenCli = ?, nomCli = ?, mailCli = ?, dateinscriptionCli = NOW(), mdpCli = ?, confirmation_token = ?, ipCli = ?, telCli = ?, fixeCli = ?, villeCli = ?, adresseCli = ?, cpCli = ?, sexeCli = ?");
+        $req = $pdo->prepare("INSERT INTO clients SET pseudoCli = ?, prenCli = ?, nomCli = ?, mailCli = ?, dateinscriptionCli = NOW(), mdpCli = ?, confirmation_token = ?, ipCli = ?, telCli = ?, fixeCli = ?, villeCli = ?, adresseCli = ?, complementadrsCli = ?, cpCli = ?, sexeCli = ?");
 
         $prenom = ucfirst($_POST['prenom']);
         $nom = ucfirst($_POST['nomdefamille']);
@@ -106,11 +113,11 @@ if(empty(!$_POST)){
         $pseudo = strtolower($_POST['nomdefamille']).".".strtolower($_POST['prenom']);
 
         $client_id = $pdo->lastInsertId(); // Dernier ID généré par PDO
-        $req->execute([$pseudo, $prenom, $nom, $_POST['mail'], $password, $token, $_SERVER['REMOTE_ADDR'], $_POST['numportable'], $_POST['numfixe'], $_POST['ville'], $_POST['adressepostale'], $_POST['codepostal'], $_POST['sexe']]);
+        $req->execute([$pseudo, $prenom, $nom, $_POST['mail'], $password, $token, $_SERVER['REMOTE_ADDR'], $_POST['numportable'], $_POST['numfixe'], $_POST['ville'], $_POST['adressepostale'], $_POST['complement-adresse'], $_POST['codepostal'], $_POST['sexe']]);
 
         
 
-        echo("Votre compte a bien été créé. <br/> Voici votre lien : <br />  <a>http://localhost/espace_clients/confirm.php?id=$client_id&token=$token</a>");
+        //mail("Votre compte a bien été créé. <br/> Voici votre lien : <br />  <a>http://localhost/espace_clients/confirm.php?id=$client_id&token=$token</a>");
 
         $_SESSION['flash']['succes'] = "Votre compte a bien été créé. Un mail de confirmation vous a été envoyé.";
 
@@ -144,60 +151,65 @@ if(empty(!$_POST)){
 	
 	<div id="label1">
     <label for="nomdefamille">Votre nom de famille<span class="champ-obligatoire">*</span> : </label>
-    <input type="text" name="nomdefamille" placeholder="Nom de famille" maxlength="100" /><br /><br />
+    <input type="text" name="nomdefamille" value="<?php if (isset($_POST['nomdefamille'])){echo $_POST['nomdefamille'];} ?>" placeholder="Nom de famille" maxlength="100" required /><br /><br />
     </div>
 
     <div id="label0">
     <label for="prenom">Votre prénom<span class="champ-obligatoire">*</span> : </label>
-    <input type="text" name="prenom" placeholder="Prénom" maxlength="100" /><br /><br />
+    <input type="text" name="prenom" value="<?php if (isset($_POST['prenom'])){echo $_POST['prenom'];} ?>" placeholder="Prénom" maxlength="100" required /><br /><br />
     </div>
 
     <div id="label1">
     <label for="email">Votre adresse e-mail<span class="champ-obligatoire">*</span> : </label>
-    <input type="text" name="mail" placeholder="Adresse e-mail" maxlength="100" /><br /><br />
+    <input type="text" name="mail" placeholder="Adresse e-mail" value="<?php if (isset($_POST['mail'])){echo $_POST['mail'];} ?>" maxlength="100" required /><br /><br />
 	</div>
 	
 	<div id="label0">
     <label for="mdp">Votre mot de passe<span class="champ-obligatoire">*</span> : </label>
-    <input type="password" name="password" placeholder="Mot de passe" maxlength="256"/><br /><br />
+    <input type="password" name="password" value="<?php if (isset($_POST['password'])){echo $_POST['password'];} ?>"  placeholder="Mot de passe" maxlength="256" required/><br /><br />
 	</div>
 	
 	<div id="label1">
     <label for="mdpconfirm">Confirmez votre mot de passe<span class="champ-obligatoire">*</span> : </label>
-    <input type="password" name="password_confirm"  placeholder="Retapez votre mot de passe" maxlength="256"/><br /><br />
+    <input type="password" name="password_confirm"  placeholder="Retapez votre mot de passe" maxlength="256" required /><br /><br />
 	</div>
 
     <div id="label0">
     <label for="telephone">Votre numéro de téléphone : </label>
-    <input type="tel" name="numportable" placeholder="Numéro de téléphone" maxlength="10" /><br /><br />
+    <input type="tel" name="numportable" value="<?php if (isset($_POST['numportable'])){echo $_POST['numportable'];} ?>"  placeholder="Numéro de téléphone (exemple : 06XXXXXXXX)" maxlength="10" /><br /><br />
     </div>
 
     <div id="label1">
     <label for="telephonefixe">Votre numéro de téléphone fixe : </label>
-    <input type="tel" name="numfixe" placeholder="Numéro de fixe" maxlength="10"/><br /><br />
+    <input type="tel" name="numfixe" value="<?php if (isset($_POST['numfixe'])){echo $_POST['numfixe'];} ?>" placeholder="Numéro de fixe" maxlength="10"/><br /><br />
     </div>
 
     <div id="label0">
     <label for="adresse">Votre adresse postale<span class="champ-obligatoire">*</span> : </label>
-    <input type="text" name="adressepostale" placeholder="Adresse postale"/><br /><br />
+    <input type="text" name="adressepostale" value="<?php if (isset($_POST['adressepostale'])){echo $_POST['adressepostale'];} ?>" placeholder="Adresse postale" required /><br /><br />
     </div>
 
     <div id="label1">
+    <label for="complement-adresse">Complément d'adresse : </label>
+    <input type="text" name="complement-adresse" value="<?php if (isset($_POST['complement-adresse'])){echo $_POST['complement-adresse'];} ?>" placeholder="Complément d'adresse" maxlength="255" /><br /><br />
+    </div>
+
+    <div id="label0">
     <label for="cp">Votre code postal<span class="champ-obligatoire">*</span> : </label>
-    <input type="text" name="codepostal" placeholder="Code postal" maxlength="5"/><br /><br />
-    </div>
-
-    <div id="label0">
-    <label for="ville">Votre ville<span class="champ-obligatoire">*</span> : </label>
-    <input type="text" name="ville" placeholder="Ville" maxlength="100"/><br /><br />
+    <input type="text" pattern=".{5,}"  required title="5 caractères" name="codepostal" value="<?php if (isset($_POST['codepostal'])){echo $_POST['codepostal'];} ?>" placeholder="Code postal" maxlength="5" required /><br /><br />
     </div>
 
     <div id="label1">
-    <label for="datenaissance">Votre date de naissance<span class="champ-obligatoire">*</span> : </label>
-    <input type="date" name="datenaissance"/><br /><br />
+    <label for="ville">Votre ville<span class="champ-obligatoire">*</span> : </label>
+    <input type="text" name="ville" value="<?php if (isset($_POST['ville'])){echo $_POST['ville'];} ?>" placeholder="Ville" maxlength="100" required /><br /><br />
     </div>
 
     <div id="label0">
+    <label for="datenaissance">Votre date de naissance<span class="champ-obligatoire">*</span> : </label>
+    <input type="date" name="datenaissance" value="<?php if (isset($_POST['datenaissance'])){echo $_POST['datenaissance'];} ?>" required/><br /><br />
+    </div>
+
+    <div id="label1">
     <label for="sexe">Vous êtes<span class="champ-obligatoire">*</span> : </label>
     <select name="sexe">
 
