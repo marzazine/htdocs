4d5b4425d102d09ca1f332f00c020385645139ef
noam chaalane <noam.chaalane@gmail.com>
2018-05-02 15:08:00 +0200

    requete simplifiée
    Simplification de la requete

--- inc/db.class.php
+++ inc/db.class.php
@@ -4,7 +4,7 @@ class DB{
 	private $username = 'rooot';
 	private $password = '';
 	private $database = 'wolvenetdb';
-	public $db;
+	private $db;
 
 	public __ construct($host = null, $username = null, $password = null, $database = null){
 		if($host != null){
@@ -16,9 +16,18 @@ class DB{
 		}
 
 		try{
-			$this->db = new PDO ('mysql:host='.$this->host.' ;dbname= '.$this->database, $this->username, $this->password, array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES UTF8'));
+			$this->db = new PDO ('mysql:host='.$this->host.' ;dbname= '.$this->database, $this->username, $this->password, array(
+				PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES UTF8'
+				PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING
+			));
 			}catch(PDOException $e){
 				die('<h1>Impossible de se connecter a la base de donnee</h1>');
 			}
+		}
 
+		public function query($sql){
+			$req =$this->db->prepare($sql);
+			$req->execute();
+			return $req->fetchAll(PDO::FETCH_OBJ);
+		}
 }		
